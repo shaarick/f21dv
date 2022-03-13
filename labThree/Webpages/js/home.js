9fdae877a1dae7d6a91188c53060d364d06e726b
Mohammad Shariq Farooqui <msf2000@hw.ac.uk>
2022-03-13 22:04:16 +0000

    add burger animation

--- labThree/Webpages/js/home.js
+++ labThree/Webpages/js/home.js
@@ -4,8 +4,10 @@ const navSlide = () => {
     const navLinks = document.querySelectorAll('.nav-links li');
 
     burger.addEventListener('click', () => {
+        // Toggle Nav
         nav.classList.toggle('nav-active');
 
+        // Animate Links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = ''
@@ -13,6 +15,9 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         })
+
+        //Burger Animation
+        burger.classList.toggle('toggle')
     });
 }
 
